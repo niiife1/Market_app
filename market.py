8161cfd69c4a9f5d18916385b676e09b60bdac27
niiife1 <Ivandemirov28@gmail.com>
2022-08-21 12:01:11 +0300

    working

--- market.py
+++ market.py
@@ -7,3 +7,12 @@ app = Flask(__name__)\
 @app.route('/')
 def home_page():
     return render_template("home.html")
+
+@app.route('/market')
+def market_page():
+    items = [
+        {"id":1, "name":"Iphone- 13 pro MaX", "barcode":"566595929595678", "price": "1400$"},
+        {"id":2, "name":"Acer-Aspire 5", "barcode":"85455655656785", "price": "1899$"},
+        {"id":3, "name":"Keyboard-Razer mamba", "barcode":"566595929595678", "price": "140$"}     
+    ]
+    return render_template("market.html", items = items )
