33e5be6a7132a492b593daa114ebd7c66fc6dd93
niiife1 <Ivandemirov28@gmail.com>
2022-08-27 14:29:43 +0300

    frist commit

---
+++ market/models.py
@@ -0,0 +1,23 @@
+from enum import unique
+from run import db
+
+
+class User(db.Model):
+    _table_args_ = {'extend_existing': True}
+    number = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=70), nullable=False, unique=True)
+    password_defense = db.Column(db.String(length=60), nullable=False)
+    budge =db.Column (db.Integer(), nullable=False, default=10000)
+    items = db.relationship('Item', backref="owned_user", lazy=True)
+    
+    
+class Item(db.Model):
+    number = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=150), nullable=False, unique=True )
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=15), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(),db.ForeignKey('user.number'))
+def __repr__(self):
+    return f"Item{self.name}"
--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from os import abort
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market2.db'
 db = SQLAlchemy(app)
-
+    
 class Item(db.Model):
     number = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=150), nullable=False, unique=True )
@@ -22,7 +22,7 @@ class Item(db.Model):
 def home_page():
     return render_template("home.html")
 
-@app.route('/market')
+@app.route('/run')
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items = items)
