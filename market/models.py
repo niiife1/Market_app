ef2781bbd4f35d2de1c138abcf888232bf949305
niiife1 <Ivandemirov28@gmail.com>
2022-08-28 19:56:56 +0300

    working on it

--- market/models.py
+++ market/models.py
@@ -1,8 +1,6 @@
-from enum import unique
 from run import db
 
 class User(db.Model):
-    
     number = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=70), nullable=False, unique=True)
@@ -11,14 +9,13 @@ class User(db.Model):
     items = db.relationship('Item', backref="owned_user", lazy=True)
     
     
-
 class Item(db.Model):
-
     number = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=150), nullable=False, unique=True )
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=15), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
     owner = db.Column(db.Integer(),db.ForeignKey('user.number'))
-def __repr__(self):
-    return f"Item{self.name}"
+    
+    def __repr__(self):
+        return f"Item{self.name}"
