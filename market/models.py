b5765bbf1012605ec00a281cabf5346d45913357
niiife1 <Ivandemirov28@gmail.com>
2022-10-25 22:05:43 +0300

    first

--- market/models.py
+++ market/models.py
@@ -1,8 +1,11 @@
-from market.__init__ import db
+from market.__init__ import db , login_manager
 from market import bcrypt
 from flask_login import UserMixin
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
